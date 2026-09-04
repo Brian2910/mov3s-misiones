98877f5ceacd79b96435471c6c3c3bebd3c7bb9f
Brian Crowley <crowley.brian2004@gmail.com>
2026-09-04 20:14:41 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import './i18n';
+import { useTranslation } from 'react-i18next';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import MissionDetailScreen from './screens/MissionDetailScreen';
+
+export type RootStackParamList = {
+  Home: undefined;
+  MissionDetail: { missionId: string };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function App() {
+  const { t } = useTranslation();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: t('missions') }}
+        />
+        <Stack.Screen
+          name="MissionDetail"
+          component={MissionDetailScreen}
+          options={{ title: t('missionDetail') }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
